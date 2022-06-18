634d31f14ba1b9a96724c9d6437c27622746c9d1
LukeDasios <luke.dasios321@gmail.com>
2022-06-18 18:31:56 -0400

    Finalized the application

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 require("dotenv").config()
 
-const cron = require("node-cron")
-
 const accountSid = process.env.TWILIO_ACCOUNT_SID
 const authToken = process.env.TWILIO_AUTH_TOKEN
 const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER
@@ -19,8 +17,8 @@ function whoIsNext(num) {
 let garbageWeek = false
 const theBoys = ["Luke", "Duncan", "Sam", "Jp"]
 const numbers = ["+16479385063", "+14168261333", "+14168447692", "+14166169331"]
-let iter = 3
-let towel = 1
+let iter = 0
+let towel = 2
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
@@ -37,66 +35,60 @@ app.get("/", (req, res) => {
 // * * * * * *
 
 app.listen(port, () => {
-  // One time message to confirm that the app is up and running
-  client.messages.create({
-    body: "App is live!",
-    from: TWILIO_PHONE_NUMBER,
-    to: "+16479385063",
-  })
+  let fullDate = new Date()
+  let day = fullDate.getDay()
+  let hour = fullDate.getHours()
 
-  // Schedule tasks to be run on the server.
-  cron.schedule("0 18 * * 0,2,4,6", () => {
-    message()
-  })
+  message(day, hour)
 })
 
-const message = () => {
-  let day = new Date().getDay()
-
+const message = (day, hour) => {
   // Send the correct message depending on what day it is
-  if (day === 0) {
-    // Sunday
-    client.messages.create({
-      body: `Good Evening ${theBoys[iter]}! Heads up, You're on garbage duty this week.`,
-      from: TWILIO_PHONE_NUMBER,
-      to: numbers[iter],
-    })
-  } else if (day === 2) {
-    // Tuesday
-    client.messages
-      .create({
-        body: garbageWeek
-          ? `Good Evening ${theBoys[iter]}! In case you haven't already done so already, the Recycling, Compost, and Garbage need to be taken to the curb by tonight. Cheers.`
-          : `Good Evening ${theBoys[iter]}! In case you haven't already done so already, the Recycling and Compost need to be taken to the curb by tonight. Cheers.`,
+  if (hour === 18) {
+    if (day === 0) {
+      // Sunday
+      client.messages.create({
+        body: `Good Evening ${theBoys[iter]}! Heads up, You're on garbage duty this week.`,
         from: TWILIO_PHONE_NUMBER,
         to: numbers[iter],
       })
-      .then(() => {
-        garbageWeek = !garbageWeek
-      })
-  } else if (day === 4) {
-    // Thursday
-    client.messages
-      .create({
-        body: `Good Evening ${theBoys[towels]}, It's your turn on towel duty! They need to be washed, dryed, folded, and put back in their respective drawer upstairs.`,
-        from: TWILIO_PHONE_NUMBER,
-        to: numbers[towel],
-      })
-      .then((message) => {
-        towel == 3 ? 0 : towel + 1
-      })
-  } else if (day === 6) {
-    // Saturday
-    client.messages
-      .create({
-        body: `Good Efternoon ${
-          theBoys[iter]
-        }! Please empty the Recycling, Green bin, and Garbage one last time so that ${whoIsNext()} may start their week with a clean slate. After that, you are free!`,
-        from: TWILIO_PHONE_NUMBER,
-        to: numbers[iter],
-      })
-      .then((message) => {
-        iter == 3 ? 0 : iter + 1
-      })
+    } else if (day === 2) {
+      // Tuesday
+      client.messages
+        .create({
+          body: garbageWeek
+            ? `Good Evening ${theBoys[iter]}! In case you haven't already done so already, the Recycling, Compost, and Garbage need to be taken to the curb by tonight. Cheers.`
+            : `Good Evening ${theBoys[iter]}! In case you haven't already done so already, the Recycling and Compost need to be taken to the curb by tonight. Cheers.`,
+          from: TWILIO_PHONE_NUMBER,
+          to: numbers[iter],
+        })
+        .then(() => {
+          garbageWeek = !garbageWeek
+        })
+    } else if (day === 4) {
+      // Thursday
+      client.messages
+        .create({
+          body: `Good Evening ${theBoys[towels]}, It's your turn on towel duty! They need to be washed, dryed, folded, and put back in their respective drawer upstairs.`,
+          from: TWILIO_PHONE_NUMBER,
+          to: numbers[towel],
+        })
+        .then((message) => {
+          towel == 3 ? 0 : towel + 1
+        })
+    } else if (day === 6) {
+      // Saturday
+      client.messages
+        .create({
+          body: `Good Efternoon ${
+            theBoys[iter]
+          }! Please empty the Recycling, Green bin, and Garbage one last time so that ${whoIsNext()} may start their week with a clean slate. After that, you are free!`,
+          from: TWILIO_PHONE_NUMBER,
+          to: numbers[iter],
+        })
+        .then((message) => {
+          iter == 3 ? 0 : iter + 1
+        })
+    }
   }
 }
